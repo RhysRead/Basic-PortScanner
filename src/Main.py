23d3b28755fb56dad3f47daae4fb834594a9e4d0
RhysRead <30420852+RhysRead@users.noreply.github.com>
2019-02-09 12:18:29 +0000

    Naming conventions

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""Main.py: The main file used to execute PortScanner."""
+"""main.py: The main file used to execute PortScanner."""
 
 __author__ = "Rhys Read"
 __copyright__ = "Copyright 2018, Rhys Read"
